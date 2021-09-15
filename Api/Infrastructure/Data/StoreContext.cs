b370b7677d59d289787a968cb13527a91bf2e74a
marshadkarachi1 <arshad_aptech@hotmail.com>
2021-09-15 16:33:21 +0500

    initial commit

---
+++ Api/Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+   public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
